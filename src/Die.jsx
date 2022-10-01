49cb7299afa0061e2e8960979d66f966f75273a8
Tesha R <tesha@ldrdesigns.com>
2022-09-30 18:51:10 -0700

    Add Die component

---
+++ src/Die.jsx
@@ -0,0 +1,18 @@
+function Die(props) {
+  return (
+    <div className="die-el">
+      <div className="die-item">1</div>
+      <div className="die-item">1</div>
+      <div className="die-item">1</div>
+      <div className="die-item">1</div>
+      <div className="die-item">1</div>
+      <div className="die-item">1</div>
+      <div className="die-item">1</div>
+      <div className="die-item">1</div>
+      <div className="die-item">1</div>
+      <div className="die-item">1</div>
+    </div>
+  );
+}
+
+export default Die;
