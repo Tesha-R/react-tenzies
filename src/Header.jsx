68f98dada3961c27b22eaf78be2eb26d181fe45a
Tesha R <tesha@ldrdesigns.com>
2022-10-03 11:11:06 -0700

    Style Header component

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,8 +1,8 @@
 function Header() {
   return (
     <div className="header-el">
-      <h1>Tenzies</h1>
-      <p>
+      <h1 className="header-title">Tenzies</h1>
+      <p className="header-desc">
         Roll until all dice are the same. Click each die to freeze it at its
         current value between rolls.
       </p>
