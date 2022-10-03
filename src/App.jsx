091971253229ff563f254826f791b5bf3449da97
Tesha R <tesha@ldrdesigns.com>
2022-10-03 13:42:11 -0700

    Change dice to objects and add unique id

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,40 @@
 import { useState } from 'react';
+import { nanoid } from 'nanoid';
 import Die from './Die';
 import Header from './Header';
 
 function App() {
   const [diceArray, setDiceArray] = useState(allNewDice);
 
-  const singleDie = diceArray.map((dice) => {
-    return <Die value={dice} />;
-  });
+  function handleRollDice() {
+    setDiceArray(allNewDice);
+  }
 
   //  array of 10 random numbers between 1-6
   function allNewDice() {
     const array = [];
     for (let i = 0; i < 10; i++) {
       let randomNum = Math.floor(Math.random() * 6 + 1);
-      array.push(randomNum);
+      array.push({
+        value: randomNum,
+        isHeld: false,
+        id: nanoid(),
+      });
     }
     return array;
   }
+
+  const singleDie = diceArray.map((dice) => {
+    return <Die key={Die.id} value={dice.value} />;
+  });
 
   return (
     <div className="main-el">
       <Header />
       <div className="die-el">{singleDie}</div>
+      <button className="die-btn" onClick={handleRollDice}>
+        Roll
+      </button>
     </div>
   );
 }
