2ab6224ca37d5e6efc61f8a2386181809a53fcd5
Tesha R <tesha@ldrdesigns.com>
2022-10-03 12:18:38 -0700

    Map array to Die component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,28 @@
+import { useState } from 'react';
 import Die from './Die';
 import Header from './Header';
 
 function App() {
+  const [diceArray, setDiceArray] = useState(allNewDice);
+
+  const singleDie = diceArray.map((dice) => {
+    return <Die value={dice} />;
+  });
+
+  //  array of 10 random numbers between 1-6
+  function allNewDice() {
+    const array = [];
+    for (let i = 0; i < 10; i++) {
+      let randomNum = Math.floor(Math.random() * 6 + 1);
+      array.push(randomNum);
+    }
+    return array;
+  }
+
   return (
     <div className="main-el">
       <Header />
-      <div className="die-el">
-        <Die value={1} />
-        <Die value={2} />
-        <Die value={3} />
-        <Die value={4} />
-        <Die value={5} />
-        <Die value={6} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-      </div>
+      <div className="die-el">{singleDie}</div>
     </div>
   );
 }
